8f276d47126c6449dd6ff293ddab390a8960598b
thejaswini.kotturap <thejaswini.kotturap@zones.com>
2023-09-17 20:02:45 +0530

    Add improve text suggestion block

--- src/Components/EventService.js
+++ src/Components/EventService.js
@@ -1,20 +1,18 @@
 import apiService from "../Hooks/apiService";
 
-export default{
+export default {
+  fetchEvents: async (date) => {
+    const response = await apiService.get(`shows?date=${date}`);
+    return response.data;
+  },
 
-    fetchEvents: async (date) => {
-        const response = await apiService.get(`shows?date=${date}`);
-        return response.data;
-    },
+  getSuggestions: async (payload) => {
+    const response = await apiService.post("events/improve", payload);
+    return response.data;
+  },
 
-    getSuggestions:  async (date) => {
-        const response = await apiService.get(`path=${date}`);
-        return response.data;
-    },
-
-    create: async (payload) => {
-        const response = await apiService.post("events", payload);
-        return response.data;
-    }
-
-}
+  create: async (payload) => {
+    const response = await apiService.post("events", payload);
+    return response.data;
+  },
+};
